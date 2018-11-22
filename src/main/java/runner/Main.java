bf63cac11913d248b6e37750825eabb94ebb3f5a
Alexey Kosarevski <alekseykosarevski@gmail.com>
2018-11-23 02:15:00 +0300

    issue checklist

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Triangle tr = new Triangle((	Double.POSITIVE_INFINITY - 1), (	Double.POSITIVE_INFINITY -1), (	Double.POSITIVE_INFINITY -1));
+        Triangle tr = new Triangle((	1), (	1), (	Math.sqrt(2)));
         System.out.println(tr.checkTriangle());
         System.out.println(tr.getMessage());
         System.out.println(tr.detectTriangle());
